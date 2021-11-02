8617b1c572014b3ead25d523cd2e68155a7583d1
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-02 16:30:23 -0400

    add route to editor

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Editor from './Components/Editor/Editor';
 
 function App() {
 
-  //states 
   const [data, setData] = useState([])
-
   const [username, setUsername] = useState('')
-
+  const [template, setTemplate] = useState('')
   const appFunctions = { setUsername }
 
   useEffect(() => {
@@ -29,6 +28,9 @@ function App() {
     <Router>
       <Login username={username} />
       <Switch>
+        <Route path="/editor">
+          <Editor username={username} appFunctions={appFunctions} template={template} />
+        </Route>
         <Route path="/">
           <Home username={username} appFunctions={appFunctions} />
         </Route>
