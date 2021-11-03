3ad4cf0da2f5c7377a7b750a6e4131a26617a2ad
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-03 16:55:42 -0400

    added button to access dashboard

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { Button, Typography } from '@mui/material';
+import { Button, FormControl, Typography } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { Box } from '@mui/system';
@@ -8,6 +8,9 @@ import { useContext, useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { AppFunctionsContext, UserDataContext } from '../../App';
 import video from './backdrop.mp4'
+import { Paper } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Home() {
 
@@ -25,7 +28,7 @@ export default function Home() {
 
     useEffect(() => { setTemplateNameValid(templates.includes(templateName)) }, [templateName])
 
-    const onSubmit = (e) => {
+    const onSubmitName = (e) => {
         e.preventDefault();
         setDisplayAutocomplete(true)
         appFunctions.login(usernameField)
@@ -38,12 +41,16 @@ export default function Home() {
 
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
-            onSubmit(e)
+            onSubmitName(e)
         }
     }
 
     const goToEditor = (e) => {
         history.push(`/editor?template=${templateName}`)
+    }
+
+    function goToDashboard() {
+        history.push(`/dashboard`)
     }
 
     return (
@@ -58,51 +65,68 @@ export default function Home() {
             }}>
                 <source src={video} type='video/mp4' />
             </video>
-            <Box sx={{ m: 30 }}>
-                {displayAutocomplete && userData ?
-                    <div>
-                        <Box display='inline-flex' alignItems='center'>
-                            <Box width={80}>
-                                <Button variant="text" aria-label="back" onClick={(e) => closeAutoComplete(e)} >
-                                    <ArrowBackIcon sx={{ mr: 1 }} />
-                                </Button>
-                            </Box>
-                            <Box>
-                                Welcome, {userData.user_name}!
-                            </Box>
-                            <Box width={80} />
-                        </Box>
-                        <Autocomplete
-                            disablePortal
-                            id="template-autocomplete"
-                            options={templates}
-                            inputValue={templateName}
-                            data-testid="autocomplete"
-                            onInputChange={(event, newInputValue) => {
-                                setTemplateName(newInputValue);
-                            }}
-                            sx={{ width: 300, my: 4, mx: "auto" }}
-                            renderInput={(params) => <TextField {...params} label="What would you like to draft?" />}
-                        />
-                        {templateNameValid ?
-                            <Box display='inline-flex' alignItems='center'>
-                                <Box width={80} />
-                                <Box>
-                                    Let's go!
-                                </Box>
-                                <Box width={80} > <Button variant="text" aria-label="go" onClick={(e) => goToEditor(e)} >
-
-                                    <ArrowForwardIcon sx={{ ml: 1 }} />
-                                </Button></Box>
-                            </Box>
-
+            <Grid
+                height='100%'
+                container
+                spacing={0}
+                align="center"
+                justify="center"
+                direction="column"
+            >
+                <Grid item sx={{ mx: 'auto', mt: 20 }}>
+                    <Paper sx={{ p: 5, width: 350, height: 400, backgroundColor: 'rgba(255, 255, 255, 0.9)' }}>
+                        {displayAutocomplete && userData ?
+                            <Grid spacing={4}  >
+                                <Grid item>
+                                    <Box display='inline-flex' alignItems='center'>
+                                        <Box width={80}>
+                                            <Button variant="text" aria-label="back" onClick={(e) => closeAutoComplete(e)} >
+                                                <ArrowBackIcon sx={{ mr: 1 }} />
+                                            </Button>
+                                        </Box>
+                                        <Box>
+                                            Welcome back, {userData.user_name}!
+                                        </Box>
+                                        <Box width={80} />
+                                    </Box>
+                                    <Autocomplete
+                                        disablePortal
+                                        id="template-autocomplete"
+                                        options={templates}
+                                        inputValue={templateName}
+                                        data-testid="autocomplete"
+                                        onInputChange={(event, newInputValue) => {
+                                            setTemplateName(newInputValue);
+                                        }}
+                                        sx={{ width: 300, my: 4, mx: "auto" }}
+                                        renderInput={(params) => <TextField {...params} label="Would you like to start a new draft?" />}
+                                    />
+                                    {templateNameValid ?
+                                        <Button endIcon={<SendIcon />} onClick={(e) => goToEditor(e)}>
+                                            Start a new draft
+                                        </Button>
+                                        :
+                                        <></>}
+                                </Grid>
+                                <Grid item>
+                                    {userData.serializedOptions.length > 0 ?
+                                        <Button sx={{ mt: 5 }} endIcon={<SendIcon />} onClick={(e) => goToDashboard()}>
+                                            view my dashboard
+                                        </Button> : <></>}
+                                </Grid>
+                            </Grid>
                             :
-                            <></>}</div>
-                    :
-                    <Box>
-                        <TextField sx={{ mx: "auto" }} id="name-textfield" label="What is your name?" variant="standard" onChange={(e, value) => setUsernameField(e.target.value)} value={usernameField} onKeyPress={(e) => handleKeyPress(e)} />
-                    </Box>
-                }
-            </Box >
+                            <Box>
+                                <FormControl onSubmit={(e) => handleKeyPress(e)}>
+                                    <TextField sx={{ mx: "auto", my: 4 }} id="name-textfield" label="What is your name?" variant="standard" onChange={(e, value) => setUsernameField(e.target.value)} value={usernameField} onKeyPress={(e) => handleKeyPress(e)} />
+                                    <Button endIcon={<SendIcon />} onClick={(e) => onSubmitName(e)}>
+                                        Go
+                                    </Button>
+                                </FormControl>
+                            </Box>
+                        }
+                    </Paper >
+                </Grid>
+            </Grid >
         </div >)
 }
