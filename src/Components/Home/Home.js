e1bf57dbf33be8ac059b8ccacfb4583e2f03c9bf
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-05 17:23:30 -0400

    add autocomplete template list

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,33 +1,56 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { Button, FormControl, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import { Button, FormControl, Paper } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
+import Grid from '@mui/material/Grid';
+import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
-import { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { AppFunctionsContext, UserDataContext } from '../../App';
-import video from './BackdropTwo_v2.mp4'
-import mjolnirImage from './mjolnir.png'
-import { Paper } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import SendIcon from '@mui/icons-material/Send';
+import video from './BackdropTwo_v2.mp4';
+import mjolnirImage from './mjolnir.png';
 
 export default function Home() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = (e) => setOpen(true);
+    const handleClose = (e) => setOpen(false);
 
     const userData = useContext(UserDataContext)
     const appFunctions = useContext(AppFunctionsContext)
 
     const [usernameField, setUsernameField] = useState('')
     const [displayAutocomplete, setDisplayAutocomplete] = useState(false)
-    const [templateName, setTemplateName] = useState('')
-    const [templateNameValid, setTemplateNameValid] = useState(false)
-
-    const templates = ["Letter to Santa", "Breakup Letter", "Notice of Intent"]
-
+    const [selectedTemplateId, setSelectedTemplateId] = useState('')
+    const [templateNameValid, setTemplateNameValid] = useState(true)
+    const [templateTitleList, setTemplateTitleList] = useState([])
     const history = useHistory();
 
-    useEffect(() => { setTemplateNameValid(templates.includes(templateName)) }, [templateName])
+    // Fetch templates and grab the title and id to display in the dropdown
+    useEffect(() => {
+        appFunctions
+            .fetchTemplates()
+            .then(templates => {
+                console.log("templates", templates)
+                let titleList = templates.map(template => { return { label: template.templates.title, id: template.templates.id } })
+                setTemplateTitleList(titleList)
+            })
+    }, [])
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'white',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
     const onSubmitName = (e) => {
         e.preventDefault();
@@ -46,13 +69,60 @@ export default function Home() {
         }
     }
 
-    const goToEditor = (e) => {
-        history.push(`/editor?template=${templateName}`)
+    const goToEditor = () => {
+        // Get the template ID from App
+        // Push to the editor with template ID
+        history.push(`/editor?templateId=${selectedTemplateId}`)
     }
 
     function goToDashboard() {
         history.push(`/dashboard`)
     }
+
+    const handleLoginClick = (e) => {
+        e.preventDefault();
+        if (userData.username === !undefined) {
+            handleClose()
+        } else {
+            handleOpen()
+        }
+    }
+
+    const registerUser = (e) => {
+        e.preventDefault()
+        let username = e.target[0].value
+        let password = e.target[1].value
+
+        //Name of the file : sha256-hash.js
+        //Loading the crypto module in node.js
+        var crypto = require('crypto');
+        //creating hash object 
+        var hash = crypto.createHash('sha256');
+        //passing the data to be hashed
+        let data = hash.update(password, 'utf-8');
+        //Creating the hash in the required format
+        let password_hash = data.digest('hex');
+        //Printing the output on the console
+        console.log("hash : " + password_hash);
+
+        console.log('registering', username, password_hash)
+        appFunctions.postUserAccount(username, password_hash)
+    }
+
+    const autoCompleteField = <Autocomplete
+        disablePortal
+        id="template-autocomplete"
+        options={templateTitleList}
+        getOptionLabel={option => option.label}
+        data-testid="autocomplete"
+        onChange={(event, newValue) => {
+            console.log('autocompleteEvent', newValue)
+            setSelectedTemplateId(newValue.id);
+        }
+        }
+        sx={{ width: 300, my: 4, mx: "auto" }}
+        renderInput={(params) => <TextField {...params} label="Would you like to start a new draft?" />}
+    />
 
     return (
         <div className="App">
@@ -91,19 +161,8 @@ export default function Home() {
                                         </Box>
                                         <Box width={80} />
                                     </Box>
-                                    <Autocomplete
-                                        disablePortal
-                                        id="template-autocomplete"
-                                        options={templates}
-                                        inputValue={templateName}
-                                        data-testid="autocomplete"
-                                        onInputChange={(event, newInputValue) => {
-                                            setTemplateName(newInputValue);
-                                        }}
-                                        sx={{ width: 300, my: 4, mx: "auto" }}
-                                        renderInput={(params) => <TextField {...params} label="Would you like to start a new draft?" />}
-                                    />
-                                    {templateNameValid ?
+                                    {autoCompleteField}
+                                    {selectedTemplateId !== '' ?
                                         <Button endIcon={<SendIcon />} onClick={(e) => goToEditor(e)}>
                                             Start a new draft
                                         </Button>
@@ -125,10 +184,43 @@ export default function Home() {
                                         Go
                                     </Button>
                                 </FormControl>
+                                <Button endIcon={<SendIcon />} onClick={(e) => handleOpen(e)} >
+                                    New user?
+                                </Button>
                             </Box>
                         }
                     </Paper >
                 </Grid>
+                <Modal
+                    open={open}
+                    onOpen={(e) => handleLoginClick(e)}
+                    onClose={(e) => handleClose(e)}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Register
+                        </Typography>
+                        <form onSubmit={(e) => registerUser(e)}>
+                            <TextField
+                                id="filled-required"
+                                label="What do we call you?"
+                                placeholder="Username/Email"
+                                variant="filled"
+                            />
+                            <TextField
+                                required='secure-ish'
+                                id="filled-password-input"
+                                label="Secure-ish"
+                                type="password"
+                                placeholder="Password"
+                                variant="filled"
+                            />
+                            <Button type='submit'>I wanna be apart of this!</Button>
+                        </form>
+                    </Box>
+                </Modal>
             </Grid >
         </div >)
 }
