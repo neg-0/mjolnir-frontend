23d8ff6e2c8cd286a46097c13a49359dc9068921
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-05 17:24:24 -0400

    fetch real data

--- src/Components/UserDashboard/History.js
+++ src/Components/UserDashboard/History.js
@@ -5,11 +5,13 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import { UserDataContext } from '../../App';
-import { useContext } from 'react';
+import { useContext, useState, useEffect, setState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { MDBIcon } from 'mdb-react-ui-kit';
+import { AppFunctionsContext, UserDataContext } from '../../App';
+import MarkdownRenderer from '../Editor/MarkdownRenderer';
+
 import {
     MDBCarousel,
     MDBCarouselInner,
@@ -18,98 +20,102 @@ import {
     MDBCarouselCaption
 } from 'mdb-react-ui-kit';
 
-const someDamnData = [
-    {
-        formName: 'this',
-        data: '**Lots of Cool Stuff!**',
-        image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(19).jpg',
-        serializedOptions: [
-            { id: 1, title: "First Letter to Santa", name: "Little Brian" }
-        ],
-    },
-    {
-        formName: 'that',
-        data: 'lots of cool stuff sorta',
-        image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(40).jpg',
-        serializedOptions: [
-            { id: 2, title: "Second Letter to Santa", name: "Little Dustin" }
-        ],
-    },
-    {
-        formName: 'Anotha',
-        data: '**WOW we are using Markdown!**',
-        image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg',
-        serializedOptions: [
-            { id: 3, title: "Third Letter to Santa", name: "Little Floyd" }
-        ],
-    },
-    {
-        formName: 'Anotha',
-        data: '**WOW we are using Markdown!**',
-        image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(19).jpg',
-        serializedOptions: [
-            { id: 3, title: "Third Letter to Santa", name: "Little Floyd" }
-        ],
-    },
-    {
-        formName: 'Anotha',
-        data: '**WOW we are using Markdown!**',
-        image: 'https://mdbootstrap.com/img/Photos/Slides/img%20(40).jpg',
-        serializedOptions: [
-            { id: 3, title: "Third Letter to Santa", name: "Little Floyd" }
-        ],
-    }
-]
+
 
 export default function History() {
 
-    const userData = useContext(UserDataContext)
+    // const userData = useContext(UserDataContext)
+    const appFunctions = useContext(AppFunctionsContext)
 
-    //delete History forms     will be async
-    const handleDeleteHistory = (e) => {
-        //e.preventDefault();
-        // await fetch(`users/${userData.name}/history/${e.target.value}`, {
-        //     method: 'DELETE'
-        // })
-        //     .then(user => 
-        //userData(user.history);
-        // })
-        console.log('im  a delete history button...sorta')
+    const [template, setTemplate] = useState() // The original text from the markdown file
+    const [templateOptions, setTemplateOptions] = useState() // JSON object of template option names and values
+    const [serializedOptions, setSerializedOptions] = useState({}) // JSON object of user-provided options
+
+    const [userHistory, setUserHistory] = useState()
+    const userData = useContext(UserDataContext)
+    const user = userData.user_name;
+
+    useEffect(() => {
+        fetchUserHistory()
+    }, [])
+
+
+    async function fetchUserHistory() {
+        console.log("USER", user)
+        return fetch(`http://localhost:3001/users/${user}/history`)
+            .then(response => response.text())
+            .then(json => {
+                console.log("JSON", json)
+                return json
+            })
+            .then(history => setUserHistory(userHistory))
+            .catch(error => console.log(error));
     }
 
 
-    //favorites will be in userData
+    if (!template || !templateOptions) {
+        return null
+    }
+
+
+
+
+    //delete History forms
+    const handleDeleteHistory = async (e) => {
+        //     e.preventDefault();
+        //     await fetch(`users/${userData.name}/history/${e.target.value}`, {
+        //         method: 'DELETE'
+        //     })
+        //         .then(user => {
+        //             // userData(user.history);
+        //         })
+        console.log('delete req')
+    }
+
+
+
+    //edit template in history
+    const handleEditHistory = async (e) => {
+        // e.preventDefault();
+        // await fetch(`users/${userData.name}/history/${e.target.value}`, {
+        //     method: 'PATCH',
+        //     body: JSON.stringify({
+        //         history: {
+        //             history_id: e.target.value,
+        //         }
+        //     })
+        // })
+        //     .then(user => {
+        //         // userData(user.history);
+        //     })
+        console.log('edit req')
+    }
+
+
+
 
     return (
         <MDBCarousel showControls showIndicators dark fade sx={{
             height: '100vh'
         }} >
             <MDBCarouselInner>
-                {someDamnData.map((form, index) =>
-                    <MDBCarouselItem className={index === 0 ? 'active' : ''}>
-                        <MDBCarouselElement src={form.image} />
-                        <MDBCarouselCaption>
-                            <h5>{form.formName}</h5>
-                            <p>{form.data}</p>
-                            <Button onClick={(e) => handleDeleteHistory(form.serializedOptions.id)}>{<MDBIcon fas icon="trash" size='2x' />}</Button>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                )}
+                {userHistory.map(history => {
+                    return (
+                        <MDBCarouselItem className="active">
+                            <MDBCarouselElement />
+                            <MDBCarouselCaption>
+                                <h5>{history.history_id}</h5>
+                                <MarkdownRenderer template={history.template} templateOptions={history.template_options} serializedOptions={history.serialized_options} />
+                                <Button onClick={(e) => handleDeleteHistory(e)}>{<MDBIcon fas icon="trash" color="black" size='1.5x' />}</Button>
+                                <Button onClick={(e) => handleEditHistory(e)}>{<MDBIcon fas icon="pencil-alt" color="black" size='1.5x' />}</Button>
+                            </MDBCarouselCaption>
+                        </MDBCarouselItem>
+                    )
+                })}
             </MDBCarouselInner>
         </MDBCarousel >
     )
 }
 
 
-/*
-
-let user = {
-    id: 1,
-    name: brian,
-    history: [
-      {id: 1, title: "test letter", name: "Little Timmy"}
-    ],
-    favorites: [1, 2, 3]
-  }
-
-*/
+//src={<MarkdownRenderer template={template} templateOptions={templateOptions} serializedOptions={null}
